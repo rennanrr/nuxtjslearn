3c68ec637d89ed060c6326561f500c67e8b3d4e7
Rennan Ribas <rennanrr@hotmail.com>
2021-05-29 18:34:49 -0300

    03 - Working with Google

--- pages/home/_id.vue
+++ pages/home/_id.vue
@@ -1,20 +1,33 @@
 <template>
   <div>
-    <div style="display:flex;">
-      <img v-for="image in home.images" :key="image" :src="image" width="200" height="150">
+    <div style="display: flex">
+      <img
+        v-for="image in home.images"
+        :key="image"
+        :src="image"
+        width="200"
+        height="150"
+      />
     </div>
-    {{ home.title }}<br/>
+    {{ home.title }}<br />
     ${{ home.pricePerNight }}
-    <img src="/images/marker.svg" width="20" height="20"/>{{ home.location.address }} {{ home.location.city }} {{ home.location.state }} {{ home.location.country }}<br/>
-    <img src="/images/star.svg" width="20" height="20"/>{{ home.reviewValue }}
-    {{ home.guests }} guests, {{ home.bedrooms }} rooms, {{ home.beds }}, {{ home.bathrooms }} bath<br/>
+    <img src="/images/marker.svg" width="20" height="20" />{{
+      home.location.address
+    }}
+    {{ home.location.city }} {{ home.location.state }} {{ home.location.country
+    }}<br />
+    <img src="/images/star.svg" width="20" height="20" />{{ home.reviewValue }}
+    {{ home.guests }} guests, {{ home.bedrooms }} rooms, {{ home.beds }},
+    {{ home.bathrooms }} bath<br />
+    {{ home.description }}
+    <div style="height: 800px; width: 800px" ref="map"></div>
   </div>
 </template>
 
 <script>
 import homes from "~/data/homes";
 export default {
-  head(){
+  head() {
     return {
       title: this.home.title
     }
@@ -23,6 +36,12 @@ export default {
     return {
       home: {},
     };
+  },
+  methods: {
+    
+  },
+  mounted() {
+    this.$maps.showMap(this.$refs.map, this.home._geoloc.lat, this.home._geoloc.lng)  
   },
   created() {
     const home = homes.find((home) => home.objectID == this.$route.params.id);
